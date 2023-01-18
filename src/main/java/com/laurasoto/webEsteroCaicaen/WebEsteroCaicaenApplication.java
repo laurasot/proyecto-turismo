7bf55346d7d53602d467d7e0c4c031684cfd5b81
laurasot <lauraflorencia5@gmail.com>
2023-01-18 18:29:45 -0300

    nuevo repo

---
+++ src/main/java/com/laurasoto/webEsteroCaicaen/WebEsteroCaicaenApplication.java
@@ -0,0 +1,13 @@
+package com.laurasoto.webEsteroCaicaen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEsteroCaicaenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEsteroCaicaenApplication.class, args);
+	}
+
+}
